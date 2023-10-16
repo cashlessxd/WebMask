c43a176c0678be4b62fc0b03e6d6f7616116f0b3
cashlessxd <mnoel8393@gmail.com>
2023-10-16 12:45:50 +0200

    Implemented face detection for one face at the time

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let showCamButton = document.getElementById("showCam");
     let video = document.getElementById("video");
     let mediaDevices = navigator.mediaDevices;
 
-    showCamButton.addEventListener("click", () => {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+    let model;
+
+    const setUpVideo = () => {
         mediaDevices
             .getUserMedia({
                 video: true,
-                audio: true,
+                audio: false,
             })
             .then((stream) => {
                 video.srcObject = stream;
-                video.addEventListener("loadedmetadata", () => {
-                    video.play();
-                });
             })
             .catch(alert);
+    }
 
-        const canvas = document.getElementById("canvas");
-        const ctx = canvas.getContext("2d");
+    const detectFaces = async () => {
+        const prediction = await model.estimateFaces(video, false);
+        mask(prediction[0].topLeft, prediction[0].bottomRight);
+    }
+
+
+    const mask = (topLeft, bottomRight) => {
+        faceWidth = bottomRight[0] - topLeft[0];
+        faceHeight = bottomRight[1] - topLeft[1];
+
         let image = new Image();
         image.src = "img/masks/surprised_emoji.png";
         image.onload = () => {
-            ctx.drawImage(image, 10, 10, 100, 100);
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            ctx.drawImage(image, topLeft[0], topLeft[1], faceWidth, faceHeight);
+        }
+    }
+
+    setUpVideo();
+
+    video.addEventListener("loadedmetadata", async () => {
+        video.play();
+        model = await blazeface.load();
+        while (true) {
+            await detectFaces();
         }
     });
 });
