b1fda23221f4d116724ce1216953a0ae0d3ceb34
cashlessxd <mnoel8393@gmail.com>
2023-10-16 13:28:49 +0200

    Implemented mask selection and size/offset manipulation

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let model;
 
+    let selectedImage = 0;
+    const images = [
+        {
+            src: "img/masks/batman.png",
+            sizeOffsetX: 80,
+            sizeOffsetY: 150,
+            posOffsetX: 40,
+            posOffsetY: 150,
+        },
+        {
+            src: "img/masks/surprised_emoji.png",
+            sizeOffsetX: 80,
+            sizeOffsetY: 150,
+            posOffsetX: 40,
+            posOffsetY: 100,
+        }
+    ];
+
     const setUpVideo = () => {
         mediaDevices
             .getUserMedia({
@@ -27,13 +45,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const mask = (faces) => {
         let image = new Image();
-        image.src = "img/masks/surprised_emoji.png";
+        image.src = images[selectedImage].src;
         image.onload = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height)
             faces.forEach((face) => {
                 let faceWidth = face.bottomRight[0] - face.topLeft[0];
                 let faceHeight = face.bottomRight[1] - face.topLeft[1];
-                ctx.drawImage(image, face.topLeft[0], face.topLeft[1], faceWidth, faceHeight);
+                ctx.drawImage(
+                    image,
+                    face.topLeft[0] - images[selectedImage].posOffsetX,
+                    face.topLeft[1] - images[selectedImage].posOffsetY,
+                    faceWidth + images[selectedImage].sizeOffsetX,
+                    faceHeight + images[selectedImage].sizeOffsetY
+                );
             });
         }
     }
@@ -43,8 +67,12 @@ document.addEventListener("DOMContentLoaded", () => {
     video.addEventListener("loadedmetadata", async () => {
         video.play();
         model = await blazeface.load();
-        while (true) {
-            await detectFaces();
+        try {
+            while (true) {
+                await detectFaces();
+            }
+        } catch (e) {
+            console.log(e);
         }
     });
 });
