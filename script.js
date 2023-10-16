ae67845d7f033df3abd902b69e5790fcc1b38664
cashlessxd <mnoel8393@gmail.com>
2023-10-16 13:00:41 +0200

    Implemented face detection for multiple faces

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let video = document.getElementById("video");
     let mediaDevices = navigator.mediaDevices;
 
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
 
     let model;
 
@@ -20,20 +20,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const detectFaces = async () => {
-        const prediction = await model.estimateFaces(video, false);
-        mask(prediction[0].topLeft, prediction[0].bottomRight);
+        const faces = await model.estimateFaces(video, false);
+        mask(faces);
     }
 
 
-    const mask = (topLeft, bottomRight) => {
-        faceWidth = bottomRight[0] - topLeft[0];
-        faceHeight = bottomRight[1] - topLeft[1];
-
+    const mask = (faces) => {
         let image = new Image();
         image.src = "img/masks/surprised_emoji.png";
         image.onload = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height)
-            ctx.drawImage(image, topLeft[0], topLeft[1], faceWidth, faceHeight);
+            faces.forEach((face) => {
+                let faceWidth = face.bottomRight[0] - face.topLeft[0];
+                let faceHeight = face.bottomRight[1] - face.topLeft[1];
+                ctx.drawImage(image, face.topLeft[0], face.topLeft[1], faceWidth, faceHeight);
+            });
         }
     }
 
